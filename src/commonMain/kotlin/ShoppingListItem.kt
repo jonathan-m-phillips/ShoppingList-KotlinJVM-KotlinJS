f25ceef7e4e852e785a5b6bad594551ffe7e3ea7
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2021-08-17 10:44:57 -0500

    adding new project to repo

---
+++ src/commonMain/kotlin/ShoppingListItem.kt
@@ -0,0 +1,16 @@
+import kotlinx.serialization.Serializable
+
+val shoppingList = mutableListOf(
+    ShoppingListItem("Cucumbers 🥒", 1),
+    ShoppingListItem("Tomatoes 🍅", 2),
+    ShoppingListItem("Orange Juice 🍊", 3)
+)
+
+@Serializable
+data class ShoppingListItem(val desc: String, val priority: Int) {
+    val id: Int = desc.hashCode()
+
+    companion object {
+        const val path = "/shoppingList"
+    }
+}
